d744cc29bc3fc7f8047ca539a93dfe45f624e691
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-11-14 12:58:06 +0100

    added some tests

---
+++ series-1/src/main/test-code/volume/CommentsOnlyFile.java
@@ -0,0 +1,10 @@
+// One line of comment
+/** Other line of comment */
+/** start of a multi line comment line
+* comment line
+* comment line
+* comment line
+ */
+/* line
+dddd
+*/
---
+++ series-1/src/main/test-code/volume/SampleJavaClass.java
@@ -0,0 +1,9 @@
+// Should return 5 actual lines of code 
+public class SampleJavaClass {
+  int x = 5;
+
+  public static void main(String[] args) {
+    SampleJavaClass myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
